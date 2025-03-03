ac48d52df3d3d1622a688a16cf3625be5599f206
PanyawutS <panyawut.s@kkumail.com>
2025-03-03 17:01:30 +0700

    Updates

---
+++ precache-manifest.23bfdf6770d7e7ff9f144c02fa17215d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a84e638e04da9666e88b28fabef1ff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "18616e0ad14b8f6a8151",
+    "url": "./static/css/main.3d0d6d05.chunk.css"
+  },
+  {
+    "revision": "8d02dcf78b9e12d90ea9",
+    "url": "./static/js/2.e84c92ed.chunk.js"
+  },
+  {
+    "revision": "307c327004d441f31681bcdb7b6b7654",
+    "url": "./static/js/2.e84c92ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18616e0ad14b8f6a8151",
+    "url": "./static/js/main.56d448fd.chunk.js"
+  },
+  {
+    "revision": "fc3b7d654d0c9983890d",
+    "url": "./static/js/runtime-main.2f33feb8.js"
+  }
+]);
